8fa350ab80b8616326155e729e472e99636d5b7b
Dennis Hoppe <dennis.hoppe@dhoppe.it>
2025-04-09 18:50:51 +0200

    fix(terraform): Ignore changes to mirror_branch_regex

--- main.tf
+++ main.tf
@@ -6,4 +6,10 @@ resource "gitlab_project_mirror" "this" {
   enabled                 = var.enabled
   keep_divergent_refs     = var.keep_divergent_refs
   only_protected_branches = var.only_protected_branches
+
+  lifecycle {
+    ignore_changes = [
+      mirror_branch_regex
+    ]
+  }
 }
